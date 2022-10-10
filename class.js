ecde688f3214498381e44b1d0abf2b641b62c13c
gautamsen117 <gautamsen117@gmail.com>
2022-10-10 11:40:00 +0600

    class.js

---
+++ class.js
@@ -0,0 +1,25 @@
+// syntactic sugar
+class Instructor {
+    name;
+    designation = 'Web Course Instructor'
+    team = 'Web team'
+    location;
+    constructor(name, location){
+        this.name = name;
+        this.location = location;
+    }
+    startSupportSession(time){
+        console.log(`start the support session at ${time}`)
+    }
+    createQuiz(module){
+        console.log(`please create quiz for module ${module}`)
+    }
+}
+
+const aamir = new Instructor('aamir', 'mumbai');
+console.log(aamir);
+aamir.startSupportSession('9.00');
+aamir.createQuiz(60);
+
+const solaiman = new Instructor('Solaiman', 'Dhaka');
+console.log(solaiman);
