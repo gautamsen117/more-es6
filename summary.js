fbab86e98a8b7a0e9d86d1532bd4fd93eaea11be
gautamsen117 <gautamsen117@gmail.com>
2022-10-10 12:23:31 +0600

    summary.js

---
+++ summary.js
@@ -0,0 +1,14 @@
+// 1. Optional chaining
+// student.marks?.math
+
+// must
+// 2. map, filter, find
+
+// 3. (Optional): forEach, reduce(advance)
+
+// 4. class
+
+// 5.(Optional): inheritance, super
+
+// 6. (must) prototypical Inheritance
+
